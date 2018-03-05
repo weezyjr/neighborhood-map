1fc01f21baa754d5006d4c6116ede5a8db787ac5
weezyjr <w33zy.7@gmail.com>
2018-03-05 19:03:34 +0200

    Add Header

--- js/app.js
+++ js/app.js
@@ -13,7 +13,25 @@ function initMap() {
 }
 
 class ViewModel {
+	constructor() {
+		//Flag to check out the current state of the menu
+		this.menu_opened = true;
+		//menu -> burger icon , close-> X icon
+		this.menu_Icon = ko.observable('menu');
+	}
 
+	openMenu() {
+		this.menu_Icon('menu');
+		this.menu_opened = true;
+	}
+	closeMenu() {
+		this.menu_Icon('close');
+		this.menu_opened = false;
+	}
+	toggleMenu() {
+		this.menu_opened ?
+			this.closeMenu() : this.openMenu();
+	}
 }
 
-ko.applyBindings(new ViewModel);
+ko.applyBindings(new ViewModel());
