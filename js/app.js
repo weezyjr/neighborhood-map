3fd9083c88868c3e07cc732bb97331df7245fdae
weezyjr <w33zy.7@gmail.com>
2018-03-05 17:27:52 +0200

    Init Map
    Initialize Google Map Api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-function initMap(){
+var map;
+
+function initMap() {
+	map = new google.maps.Map(
+		$('#map')[0], {
+			center: {
+				lat: 0,
+				lng: 0
+			},
+			zoom: 2
+		}
+	);
+}
+
+class ViewModel {
 
 }
+
+ko.applyBindings(new ViewModel);
